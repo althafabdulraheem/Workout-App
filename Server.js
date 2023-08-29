50b7220803d2d844c170d7e31c095309b167de53
ALTHAF <109522801+althafabdulraheem@users.noreply.github.com>
2023-08-29 15:54:58 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express=require('express');
+const app=express();
+const WorkRoutes=require('./routes/api');
+
+app.use(express.json())
+app.use((req,res,next)=>{
+    
+    console.log(req.path)
+    console.log(req.method)
+    next();
+})
+
+app.use('/workout',WorkRoutes);
+app.listen(process.env.PORT,()=>{
+    console.log('listened');
+})
+
+app.use((req,res)=>{
+    res.send("not fountd")
+})
+
+
+
+
